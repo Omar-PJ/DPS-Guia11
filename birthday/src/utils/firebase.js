3729e3e449f78ed5d53801878991722347701476
Omar-PJ <bryanomarperez2000@gmail.com>
2021-11-05 19:40:54 -0600

    Guia 11 Y Capturas

---
+++ birthday/src/utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-x4sLyVXv-bjzx72yov8xmB-S4YgmFrU",
+  authDomain: "birthday-bbb0e.firebaseapp.com",
+  projectId: "birthday-bbb0e",
+  storageBucket: "birthday-bbb0e.appspot.com",
+  messagingSenderId: "406446799428",
+  appId: "1:406446799428:web:cf4758fd3387578b7f1a11"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
